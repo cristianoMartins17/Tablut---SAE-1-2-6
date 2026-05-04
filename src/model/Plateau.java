b855c3e02f75b51a095f46ef0d9c0ffaa182a136
ntutin5 <nathan.tutin321@gmail.com>
2026-05-04 10:00:36 +0200

    ajout boardifier

--- src/model/Plateau.java
+++ src/model/Plateau.java
@@ -133,7 +133,6 @@ public class Plateau {
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
-
         sb.append("  0 1 2 3 4 5 6 7 8\n");
 
         for (int i = 0; i < TAILLE; i++) {
