55f23c94806584c109827d4f635527a687691a36
ntutin5 <nathan.tutin321@gmail.com>
2026-05-05 09:42:28 +0200

    ajout base TablutDecider

--- src/control/TablutDecider.java
+++ src/control/TablutDecider.java
@@ -1,4 +1,35 @@
 package control;
 
-public class TablutDecider {
+
+import boardifier.control.ActionFactory;
+import boardifier.control.Controller;
+import boardifier.control.Decider;
+import boardifier.model.GameElement;
+import boardifier.model.Model;
+import boardifier.model.action.ActionList;
+import model.TablutBoard;
+import model.TablutStageModel;
+import model.Pawn;
+
+import java.awt.*;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
+
+
+public class TablutDecider extends Decider {
+
+    private static final Random loto = new Random(Calendar.getInstance().getTimeInMillis());
+
+    public TablutDecider(Model model, Controller control) {
+        super(model, control);
+    }
+
+    @Override
+    public ActionList decide() {
+        // truc pour le bot
+
+
+        return null;
+    }
 }
