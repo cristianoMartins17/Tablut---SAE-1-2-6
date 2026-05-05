2c2f37f4f2ac01e4a01272fb1d4432f7bf5cdfa4
cristianoMartins17 <cristiano.martins25@orange.fr>
2026-05-05 09:51:43 +0200

    nom de la classe plus clair pour éviter confusion avec le Boardifier (Pawn -> TablutPawn)

---
+++ src/model/TablutPawn.java
@@ -0,0 +1,32 @@
+package model;
+
+
+import boardifier.model.ElementTypes;
+import boardifier.model.GameElement;
+import boardifier.model.GameStageModel;
+
+public class TablutPawn extends GameElement {
+    private int number;
+    private int color;
+    public static final int SOLDAT = 0;
+    public static final int MOSCOVITE = 1;
+    public static final int ROI=2;
+
+
+    public TablutPawn(GameStageModel gameStage, int number, int color) {
+        super(gameStage);
+        this.number=number;
+        this.color=color;
+
+        ElementTypes.register("pawn", 50);
+        setType(50);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getColor() {
+        return color;
+    }
+}
