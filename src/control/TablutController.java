9c70b154d35d4a67e96ecf1b8540544c4496bdcd
ntutin5 <nathan.tutin321@gmail.com>
2026-05-05 09:58:19 +0200

    ajout base TablutController

--- src/control/TablutController.java
+++ src/control/TablutController.java
@@ -1,4 +1,66 @@
 package control;
 
-public class TablutController {
+import boardifier.control.ActionFactory;
+import boardifier.control.ActionPlayer;
+import boardifier.control.Controller;
+import boardifier.model.GameElement;
+import boardifier.model.ContainerElement;
+import boardifier.model.Model;
+import boardifier.model.Player;
+import boardifier.model.action.ActionList;
+import boardifier.view.View;
+import model.TablutStageModel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TablutController extends Controller{
+
+    BufferedReader consoleIn;
+    Boolean firstPlayer;
+
+    public TablutController(Model model, View view){
+        super(model, view);
+        firstPlayer = true;
+    }
+
+
+    public void stageLoop(){
+        // permet alterner les tours
+    }
+
+    private void playTurn(){
+        // surment a modifier car choix des points probablement faux
+        Player p = model.getCurrentPlayer();
+        if (p.getType() == Player.COMPUTER){
+            System.out.println("Computer Plays");
+            TablutDecider decider = new TablutDecider(model,this);
+            ActionPlayer play = new ActionPlayer(model, this, decider, null);
+            play.start();
+        } else {
+            boolean ok = false;
+            while(!ok){
+                System.out.print(p.getName() + " -> ");
+                try {
+                    String line = consoleIn.readLine();
+                    if ( line.length() == 3 ){
+                        ok = analyseAndPlay(line);
+                    }
+                    if(!ok){
+                        System.out.println("rentrez une valeur correct, réessayez");
+                    }
+                }
+                catch (IOException e){}
+            }
+        }
+    }
+
+    public void endOfTurn(){
+    }
+    private boolean analyseAndPlay(String line){
+        // analyse si un coup et correct
+        return true;
+    }
+
 }
